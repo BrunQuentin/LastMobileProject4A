338e2ea394239eabb768b2883b2f513a038e15f0
BrunQuentin <32831076+BrunQuentin@users.noreply.github.com>
2018-11-24 18:02:44 +0100

    page home 1er modification

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+//import {  Injectable } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+//
+import { AngularFireDatabase,AngularFireList} from 'angularfire2/database';
+//import { Observable } from 'rxjs';
+
+//@Injectable() //permet d'injecter notre element créer dans la firebase
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  //creation de l'objet articles vide
+  articlesRef : AngularFireList<any>;  //liste de Firebase
+  
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private db: AngularFireDatabase, // creation d'un objet db 
+    ) {
+      /*  on lie articlesRef a notre base de donnée sur Firabase
+          en lui donnant le nom  */
+      this.articlesRef = this.db.list('/articles')
+    
   }
+
 
 }
